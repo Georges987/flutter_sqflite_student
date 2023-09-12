4b56d6915276709cf61f4f366278d4851d188c0e
= <=>
2023-09-12 14:00:50 +0100

    adding padding

--- lib/components/list_stu.dart
+++ lib/components/list_stu.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: unnecessary_const
 
 import 'package:applcation_test/models/etudiant.dart';
+import 'package:applcation_test/provider/student_provider.dart';
 import 'package:applcation_test/views/edit.dart';
+import 'package:applcation_test/views/home.dart';
 import 'package:flutter/material.dart';
 
 class ListStu extends StatelessWidget {
@@ -15,18 +17,47 @@ class ListStu extends StatelessWidget {
             child: Text("Aucun étudiant enregistré"),
           )
         : ListView.separated(
+            padding: const EdgeInsets.all(10),
             itemBuilder: ((context, index) => ListTile(
-                  leading: Text(stu.elementAt(index).id.toString()),
+                  leading: Text((index + 1).toString()),
                   title: Text(stu.elementAt(index).nom),
                   trailing: IconButton(
                       icon: const Icon(Icons.delete),
                       onPressed: () {
                         showDialog(
                             context: context,
-                            builder: (context) => const AlertDialog(
+                            builder: (context) => AlertDialog(
                                   title: const Text("About"),
                                   content: const Text(
                                       "Voulez-vous vraiment supprimer cet étudiant ?"),
+                                  actions: [
+                                    TextButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: const Text("Annuler")),
+                                    TextButton(
+                                        onPressed: () {
+                                          StuProvider()
+                                              .delete(stu.elementAt(index).id);
+                                          Navigator.of(context).pop();
+                                          Navigator.of(context)
+                                              .pushAndRemoveUntil(
+                                                  MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        const Home(),
+                                                  ),
+                                                  (route) => false);
+                                          ScaffoldMessenger.of(context)
+                                              .showSnackBar(SnackBar(
+                                                  dismissDirection: DismissDirection.horizontal,
+                                                  closeIconColor: Colors.white,
+                                                  showCloseIcon: true,
+                                                  content: Text(
+                                                      "Etudiant ${stu.elementAt(index).id} supprimé")));
+                                        },
+                                        child: const Text("Supprimer"))
+                                  ],
                                 ));
                       }),
                   onTap: () => Navigator.of(context).push(MaterialPageRoute(
